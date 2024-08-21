f09761310106d9ed06fe8b96c5c49059950e4805
jydoskey <jideomotola33@gmail.com>
2024-08-21 17:31:52 +0100

    time drop calculated on selecting radio buttons

--- src/components/meter.vue
+++ src/components/meter.vue
@@ -82,9 +82,10 @@
               </div>
 
               <div class="flex flex-col gap-6">
-                <div class="inline-flex items-center" v-for="time in timetype" :name="time.id">
+                <div class="inline-flex items-center">
                   <label class="relative flex items-center p-1 rounded-full cursor-pointer">
-                    <input name="1kwh" id="kwhdrop" type="radio" :id="time.id" @click="chooseTimeDrop()" required
+                    <input name="1kWH" value="1kWH" id="kwhdrop" type="radio" v-model="timetype"
+                      @click="chooseTimeDrop()" required
                       class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-sky-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-sky-900 checked:before:bg-sky-900 hover:before:opacity-10" />
                     <span
                       class="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
@@ -94,14 +95,35 @@
                       </svg>
                     </span>
                   </label>
-                  <label class="mt-px font-light text-gray-700 cursor-pointer">
+                  <label class="mt-px font-light text-gray-700 cursor-pointer" for="1kWH">
                     <p class="block font-sans text-sm antialiased font-normal leading-normal text-gray-700">
-                      {{ time.timename }}
+                      Determine the time it will take to drop 1kwh
                     </p>
                   </label>
+                </div>
 
+                <div class="inline-flex items-center">
+                  <label class="relative flex items-center p-1 rounded-full cursor-pointer">
+                    <input name="10WH" value="10WH" id="whdrop" type="radio" v-model="timetype"
+                      @click="chooseTimeDrop()" required
+                      class="before:content[''] peer relative h-5 w-5 cursor-pointer appearance-none rounded-full border border-sky-gray-200 text-gray-900 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-sky-900 checked:before:bg-sky-900 hover:before:opacity-10" />
+                    <span
+                      class="absolute text-gray-900 transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                      <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5 text-sky-500" viewBox="0 0 16 16"
+                        fill="currentColor">
+                        <circle data-name="ellipse" cx="8" cy="8" r="8"></circle>
+                      </svg>
+                    </span>
+                  </label>
+                  <label class="mt-px font-light text-gray-700 cursor-pointer" for="10WH">
+                    <p class="block font-sans text-sm antialiased font-normal leading-normal text-gray-700">
+                      Determine the time it will take to drop 10wh
+                    </p>
+                  </label>
                 </div>
               </div>
+
+              <p v-if="timeCalc !== ''" class="block font-sans text-sm antialiased font-normal leading-normal text-gray-700">Timetaken to drop {{timetype}} is {{ timeCalc }}</p>
 
               <div class="pt-4">
                 <button type="submit"
@@ -126,10 +148,7 @@ export default {
       avgCurrent: '',
       yellowphase: '',
       voltage: 415,
-      timetype: [
-        { timename: "Determine the time it will take to drop 1kwh", id: "kwhdrop" },
-        { timename: "Determine the time it will take to drop 10wh", id: "whdrop" }
-      ]
+      timetype: ''
     }
   },
 
@@ -144,18 +163,26 @@ export default {
     },
 
     kwhTimeDrop() {
-      let oneKwhTime = 1000 / (this.voltage * this.avgCurrentCalc() * Math.sqrt(3))
+      let oneKwhTime = 1000 / (this.voltage * this.avgCurrent * Math.sqrt(3))
       this.timeCalc = oneKwhTime
       console.log(this.timeCalc)
       return this.timeCalc
     },
 
     whTimeDrop() {
-      let tenWhTime = 10 / (this.voltage * this.avgCurrentCalc() * Math.sqrt(3))
+      let tenWhTime = 10 / (this.voltage * this.avgCurrent * Math.sqrt(3))
       this.timeCalc = tenWhTime
       console.log(this.timeCalc)
       return this.timeCalc
     },
+
+    chooseTimeDrop() {
+      if (this.timetype === 'kwhdrop') {
+        this.kwhTimeDrop()
+      } else {
+        this.whTimeDrop()
+      }
+    }
   },
 }
 </script>
